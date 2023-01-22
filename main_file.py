70cdbcac1572ad0ead5c04c201352caa81439377
John <johnnymul346@gmail.com>
2023-01-22 21:08:19 +0000

    Update main_file.py

--- main_file.py
+++ main_file.py
@@ -5,6 +5,6 @@ import numpy as np
 
 st.title('Sales Data Analysis & Report')
 
-all_data = pd.read_csv("Sales_Data_Analysis/Combined_sales_data.csv")
+all_data = pd.read_csv(r"Sales_Data_Analysis/Combined_sales_data.csv")
 
 all_data.head()
