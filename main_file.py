e69f231adfa8ca4725523f7dec367f58261d1922
John <johnnymul346@gmail.com>
2023-01-22 21:20:03 +0000

    Update main_file.py

--- main_file.py
+++ main_file.py
@@ -9,5 +9,8 @@ st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
 }))
-all_data = pd.read_csv("/Sales_Data_Analysis/Combined_sales_data.csv")
-st.write(all_data) 
+
+path = 'Combined_sales_data.csv'
+df = pd.read_csv(path, encoding='ISO-8859-1')
+
+st.write(df) 
